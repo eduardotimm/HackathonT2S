7ce86d43bd8e438e1f68013df38fc07f26f75616
eduardotimm <etimmbuonacura@gmail.com>
2025-10-07 20:42:37 -0300

    Add Rating and Report models with relevant properties and justifications

--- Back-end/Models/User.cs
+++ Back-end/Models/User.cs
@@ -2,9 +2,10 @@
 {
     public class User
     {
-        public int  UserID { get; set; }
+        public int UserID { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty; // Armazene o hash da senha
         public string Role { get; set; } = "User";
         public ICollection<Project> Projects { get; set; } = new List<Project>();
     }
