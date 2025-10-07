7926064e9147043efd31e46cb277407799c95b11
eduardotimm <etimmbuonacura@gmail.com>
2025-10-07 20:30:25 -0300

    Refactor Project model to initialize RepoURL with an empty string

--- Back-end/Models/Project.cs
+++ Back-end/Models/Project.cs
@@ -5,8 +5,10 @@
         public int ProjectID { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
-        public string RepoURL { get; set; }
-
-
+        public string RepoURL { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pendente";
+        public double? Score { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public string? MarkdownReport { get; set; }
     }
 }
