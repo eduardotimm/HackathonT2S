b2899f58617a23ca2062a9e3c6c8ccb325c61fc3
eduardotimm <etimmbuonacura@gmail.com>
2025-10-07 21:55:31 -0300

    Adicionadas e removidas propriedades do Project.cs

--- Back-end/Models/Project.cs
+++ Back-end/Models/Project.cs
@@ -2,15 +2,17 @@
 {
     public class Project
     {
-        
+
         public int ProjectID { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
         public string RepoURL { get; set; } = string.Empty;
         public string Status { get; set; } = "Pendente";
-        public double? Score { get; set; }
         public DateTime SubmittedAt { get; set; }
-        public string? MarkdownReport { get; set; }
-        public ICollection<Rating> Ratings { get; set; } = new List<Rating>();
+        public DateTime LastUpdatedAt { get; set; }
+        // Um projeto pode ter vários relatórios (re-análises)
+        public ICollection<Report> Reports { get; set; } = new List<Report>();
     }
 }
