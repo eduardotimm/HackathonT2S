336f66356737854c83ac3229638b8823851b8c9b
arthurAlberto04 <arturalberto7@gmail.com>
2025-10-07 21:19:31 -0300

    Configuração DBContext,AppSetingsJSOn e Program com EFCore

---
+++ Back-end/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HackathonT2S.Models;
+
+
+
+namespace HackathonT2S.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+    }
+}
--- Back-end/Program.cs
+++ Back-end/Program.cs
@@ -1,9 +1,19 @@
+using HackathonT2S.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Configuração DBContext
+builder.Services.AddDbContext<AppDBContext>(options => options.UseMySql(
+    builder.Configuration.GetConnectionString("Default Connection"),
+    new MySqlServerVersion(new Version(8,0,36))
+    )
+);
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
